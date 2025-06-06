4954a4e5ddc7d3cc917ffee242036a0d5d629b93
gyoolgal <mjuliapark@gmail.com>
2025-06-06 13:14:30 +1000

    uploading website

---
+++ class-changes.js
@@ -0,0 +1,30 @@
+const classChanges = [
+  // { time: 6, target: ".quote", add: "text-blue" },
+  // { time: 8, target: ".quote", remove: "text-blue" },
+
+  // { time: 6, target: ".quote1", add: "???" },
+  // { time: 36, target: ".quote2", add: "???" },
+
+  { time: 5, target: ".pop1", add: "active" },
+  { time: 36, target: ".pop2", add: "active" },
+  { time: 49, target: ".pop3", add: "active" },
+
+  { time: 0.2, target: ".p1", add: "active" },
+  { time: 22, target: ".p2", add: "active" },
+  { time: 31, target: ".p3", add: "active" },
+  { time: 51, target: ".p4", add: "active" },
+  { time: 56, target: ".p5", add: "active" },
+  // { time: ??, target: ".p6", add: "active" },
+
+
+
+  { time: 36, target: ".mainpage", add: "active" },
+  { time: 38, target: ".mainpage", remove: "active" },
+
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
