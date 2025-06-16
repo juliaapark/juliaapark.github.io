2706d3c2d18b3f90bbd6d530a135e6b937c254a5
gyoolgal <mjuliapark@gmail.com>
2025-06-16 19:34:04 +1000

    website update

--- class-changes.js
+++ class-changes.js
@@ -5,26 +5,47 @@ const classChanges = [
   // { time: 6, target: ".quote1", add: "???" },
   // { time: 36, target: ".quote2", add: "???" },
 
-  { time: 5, target: ".pop1", add: "active" },
-  { time: 36, target: ".pop2", add: "active" },
-  { time: 49, target: ".pop3", add: "active" },
+//quotes
+  { time: 6, target: ".b1", add: "active" },
+  { time: 29, target: ".b2", add: "active" },
+  { time: 36, target: ".b3", add: "active" },
+  { time: 51, target: ".b4", add: "active" },
+  { time: 118, target: ".b5", add: "active" },
+  { time: 175, target: ".b6", add: "active" },
+  { time: 295, target: ".b7", add: "active" },
+  { time: 419, target: ".b8", add: "active" },
+  { time: 442, target: ".b9", add: "active" },
+  { time: 241.5, target: ".b10", add: "active" },
 
+//poor images
   { time: 0.2, target: ".p1", add: "active" },
   { time: 22, target: ".p2", add: "active" },
   { time: 31, target: ".p3", add: "active" },
   { time: 51, target: ".p4", add: "active" },
   { time: 56, target: ".p5", add: "active" },
-  // { time: ??, target: ".p6", add: "active" },
+  { time: 89, target: ".p6", add: "active" },
+  { time: 106, target: ".p7", add: "active" },
+  { time: 117, target: ".p8", add: "active" },
+  { time: 142, target: ".p9", add: "active" },
+  { time: 240, target: ".p10", add: "active" },
+  { time: 245, target: ".p11", add: "active" },
+  { time: 274, target: ".p12", add: "active" },
+  { time: 296, target: ".p13", add: "active" },
+  { time: 303.5, target: ".p14", add: "active" },
+  { time: 321, target: ".p15", add: "active" },
+  { time: 323, target: ".p16", add: "active" },
+  { time: 327, target: ".p17", add: "active" },
+  { time: 334, target: ".p18", add: "active" },
+  { time: 357, target: ".p19", add: "active" },
+  { time: 362, target: ".p20", add: "active" },
+  { time: 369.5, target: ".p21", add: "active" },
+  { time: 390, target: ".p22", add: "active" },
+  { time: 417, target: ".p23", add: "active" },
+  { time: 425, target: ".p24", add: "active" },
 
 
-
-  { time: 36, target: ".mainpage", add: "active" },
-  { time: 38, target: ".mainpage", remove: "active" },
-
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
-
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
+//blur background
+  { time: 35, target: ".mainpage", add: "active" },
+  { time: 39, target: ".mainpage", remove: "active" },
+  { time: 442, target: ".mainpage", add: "active" },
 ];
